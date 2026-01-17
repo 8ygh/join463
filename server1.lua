86efe53b7b181889a94dfceedaca4c400d03fc9e
8ygh <162633119+8ygh@users.noreply.github.com>
2026-01-17 18:46:41 +0300

    Refactor TeleportService call to inline method

--- server1.lua
+++ server1.lua
@@ -1,6 +1,1 @@
-local TeleportService = game:GetService("TeleportService")
-
-local placeId = 131623223084840
-local jobId = "545ea34e-152c-4151-9b3d-b13cd90fda39"
-
-TeleportService:TeleportToPlaceInstance(placeId, jobId)
+game:GetService("TeleportService"):TeleportToPlaceInstance(131623223084840, "545ea34e-152c-4151-9b3d-b13cd90fda39")
