42cdd3e34e5af452f3525cf2fedb27d8770a09de
8ygh <162633119+8ygh@users.noreply.github.com>
2026-01-17 18:40:19 +0300

    Add teleportation functionality in server1.lua

---
+++ server1.lua
@@ -0,0 +1,6 @@
+local TeleportService = game:GetService("TeleportService")
+
+local placeId = 131623223084840
+local jobId = "545ea34e-152c-4151-9b3d-b13cd90fda39"
+
+TeleportService:TeleportToPlaceInstance(placeId, jobId)
